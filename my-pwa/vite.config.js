1af6e96209748602e2cfe1a7ed123c4a1b60274e
Thibaut Marmey <tm@nextepsolutions.be>
2026-03-19 14:17:08 +0100

    init

---
+++ my-pwa/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My PWA',
+        short_name: 'MyPWA',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
+})
